6cce40fa9d9035d95a375a4e1db6ce7485d986d2
Diogo <45275502+perkier@users.noreply.github.com>
2019-10-14 20:36:57 +0100

    Update Room_Heating.py

--- Thermal_Solutions/Room_Heating.py
+++ Thermal_Solutions/Room_Heating.py
@@ -1,4 +1,5 @@
 from CoolProp.CoolProp import PropsSI, PhaseSI, HAPropsSI
+import numpy as np
 
 def room_conditions():
 
@@ -49,8 +50,8 @@ def Nominal_values(fluid, P_evap, P_cond, T_evap, DELTAT_sh, eta_comp):
     # Calculo da potencia maxima [W] que a bomba de calor deve ter
     Pot_Nominal = h_sala * A_sala * ( 24 - 0.1 )
 
-    # Calculo do caudal mássico [kg/s] do fluido na bomba de calor
-    m_dot_fl =  Pot_Nominal / (h_simp[1] - h_simp[0])
+    # Calculo do caudal mássico [kg/h] do fluido na bomba de calor
+    m_dot_fl =  Pot_Nominal / (h_simp[1] - h_simp[0]) * 3600
 
     return Pot_Nominal, m_dot_fl
 
@@ -107,38 +108,56 @@ def evaporator(m_dot_fluid, T_amb, fluid):
 
     P_evap = PropsSI('P','T', T_evap + 273.15,'Q',1,fluid)
 
+    # Get m_dot_air in evaporators [kg/h]
     v_air = 3
-    m_dot_air = v_air * A_frontal * 0.8
+    m_dot_air = v_air * A_frontal * 0.8 * 3600
 
     return P_evap, T_evap
 
 
 def amb_temp():
 
-    T_amb = [5.1, 4.8, 3.5, 10]
+    T_amb = [5.1, 4.8, 3.5, 2.1, 0.8, 0.1, 1.9, 3, 5.1, 7.0, 8.2, 9.6, 10.5, 12, 12.1, 11, 8.5, 6.3, 5.2, 4.9, 4.5, 3.9, 4.1, 4.5]
 
     return T_amb
 
 
-
-def heat_room(Q_dot, T_amb, i):
+def heat_room(Q_dot, T_amb, i, T_int, Pot):
 
     A_sala, h_sala, V_sala, P_int = room_conditions()
 
-    # Caudal Volumico de ar renovado
-    V_dot_renov = 0.7 * (10 ^ (-3)) / 3600
+    # Caudal Volumico de ar renovado - [m^3 / h]
+    V_dot_renov = 0.7 * (10 ^ (-3))
 
     # CP do ar renovado
     cp_renov = PropsSI('CPMASS','T', T_amb[i] + 273.15,'P',P_int, 'Air')
 
-    # Caudal Massico de ar Renovado
+    # Caudal Massico de ar Renovado - [kg^3 / h]
     m_dot_renov = V_dot_renov * PropsSI('D','T', T_amb[i] + 273.15,'P',P_int, 'Air')
 
-    # Caudal Massico da Sala
-    m_dot_int = V_sala * PropsSI('D','T', T_amb[i] + 273.15,'P',P_int, 'Air')
+    # Caudal Massico da Sala - [kg^3 / h]
+    m_dot_int = V_sala * PropsSI('D','T', T_amb[i] + 273.15,'P',P_int, 'Air') * 3600
 
-    print(Q_dot)
-    print(T_amb[i]) 
+    Pot[i] = pump(T_int[i-1], Pot[i-1])
+
+    C_1 = m_dot_int * PropsSI('CPMASS','T', (T_int[i-1]) + 273.15,'P',P_int, 'Air')
+    C_2 = - (A_sala * h_sala + m_dot_renov * PropsSI('CPMASS', 'T', (T_amb[i] + T_int[i-1]) / 2 + 273.15, 'P', P_int, 'Air'))
+    C_3 = Pot[i]
+    C_4 = T_int[i-1]
+    C_5 = T_amb[i]
+
+    a = np.array([[(C_2 / C_1), 1, 0],
+                  [0, 1, -1],
+                  [1, 0, -1]])
+
+    b = np.array([C_3/C_1, -C_4, -C_5])
+
+    x = np.linalg.solve(a, b)
+
+    T_int[i] = x[2]
+    print(T_int[i])
+
+
 
     # Transferencia de calor entre inicio da sala e final da sala
     # heat_ex_1 = m_dot_int * PropsSI('CPMASS','T', (T_int[i]+T_int[i-1])/2 + 273.15,'P',P_int, 'Air') * (T_int[i] - T_int[i-1])
@@ -150,23 +169,48 @@ def heat_room(Q_dot, T_amb, i):
     # heat_ex_1 = m_dot_renov * PropsSI('CPMASS', 'T', (T_amb[i] + T_int[i]) / 2 + 273.15, 'P', P_int, 'Air') * (T_int[i] - T_amb[i])
 
 
+def pump(T, Pot):
+
+    T_max = 24
+    T_min = 22
+
+    if T >= T_max:
+        # Stop the heating system
+        return 0
+
+    else:
+
+        if T <= T_min:
+            # Return the maximum Power of the heating system
+            return 100000 * 3600
+
+        else:
+            return Pot
+
+
 def main():
 
     fluid = 'R125'
 
-    i = 3
-
-    T_cond, P_cond, T_evap, P_evap, Delta_T_sh, eta_comp = sub_heating(fluid)
-
-    Pot_Nominal, m_dot_fluid = Nominal_values(fluid, P_evap, P_cond, T_evap, Delta_T_sh, eta_comp)
-
     T_amb = amb_temp()
 
-    P_evap, T_evap = evaporator(m_dot_fluid, T_amb, fluid)
+    T_room = {}
+    T_room[0] = T_amb[0]
 
-    Q_dot_evap, Q_dot_cond, Q_dot_exp, Q_dot_comp = heat_states(P_evap, T_evap, P_cond, T_cond, Delta_T_sh, fluid, eta_comp, m_dot_fluid)
+    Pot = {}
+    Pot[0] = 100000 * 3600
 
-    heat_room(Q_dot_cond, T_amb, i)
+    for i in range(1, 24):
+
+        T_cond, P_cond, T_evap, P_evap, Delta_T_sh, eta_comp = sub_heating(fluid)
+
+        Pot_Nominal, m_dot_fluid = Nominal_values(fluid, P_evap, P_cond, T_evap, Delta_T_sh, eta_comp)
+
+        P_evap, T_evap = evaporator(m_dot_fluid, T_amb, fluid)
+
+        Q_dot_evap, Q_dot_cond, Q_dot_exp, Q_dot_comp = heat_states(P_evap, T_evap, P_cond, T_cond, Delta_T_sh, fluid, eta_comp, m_dot_fluid)
+
+        heat_room(Q_dot_cond, T_amb, i, T_room, Pot)
 
 
 if __name__ == '__main__':
